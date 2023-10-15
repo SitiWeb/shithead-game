1f32e99ef0efa12c0bea95b39a590c9aaaa7f985
SitiWeb <70724099+SitiWeb@users.noreply.github.com>
2023-10-15 19:17:55 +0200

    laatste dan

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,10 @@ import './bootstrap';
 import "bootstrap";
 
 import { createApp } from "vue/dist/vue.esm-bundler";
-import ExampleCounter from "./Components/ExampleCounter.vue";
 import CardList from "./Components/CardList.vue";
 import Card from "./Components/Card.vue";
 import Stack from "./Components/Stack.vue";
 const app = createApp({});
-app.component("example-counter", ExampleCounter);
 app.component("card-list", CardList);
 app.component("card", Card);
 app.component("stack", Stack);
