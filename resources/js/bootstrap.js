3098238735c78ebedf7e60c2f5cb57809e16b83f
SitiWeb <70724099+SitiWeb@users.noreply.github.com>
2023-10-15 05:08:47 +0200

    test

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -17,13 +17,12 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 import Echo from "laravel-echo";
-
+import Pusher from 'pusher-js';
 
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: '200a2bcde647700b50b3',
-    wsHost: window.location.hostname,
 
     encrypted: true,
     cluster: 'eu',
