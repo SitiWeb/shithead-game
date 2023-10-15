926d777ede3c1a17b3781129bb15e88e84ea24ef
SitiWeb <70724099+SitiWeb@users.noreply.github.com>
2023-10-15 19:15:36 +0200

    fix linux

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,10 @@ import './bootstrap';
 import "bootstrap";
 
 import { createApp } from "vue/dist/vue.esm-bundler";
-import CardList from "./components/CardList.vue";
-import Card from "./components/Card.vue";
-import Stack from "./components/Stack.vue";
+import ExampleCounter from "./Components/ExampleCounter.vue";
+import CardList from "./Components/CardList.vue";
+import Card from "./Components/Card.vue";
+import Stack from "./Components/Stack.vue";
 const app = createApp({});
 app.component("example-counter", ExampleCounter);
 app.component("card-list", CardList);
